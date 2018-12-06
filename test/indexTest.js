bf5d155e5e33ad1bd8fae7c31258fb78202833e1
kostanzhoglo <jeffreywithers@gmail.com>
2018-12-06 16:59:00 -0500

    Write first simple test in indexTest.js. Test passes

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,2 +1,8 @@
 const assert = require('chai').assert;
 const app = require('../index');
+
+describe('App', () => {
+  it('app should return "hello"', () => {
+    assert.equal(app(), "hello")
+  })
+})
